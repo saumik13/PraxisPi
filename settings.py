8745dc505edce25e8c31abf0ff3bba9021aa745c
Juho Kim <juho-kim@outlook.com>
2021-11-23 17:34:16 -0500

    added settings

---
+++ settings.py
@@ -0,0 +1,14 @@
+IMAGE_PATH = 'image.jpg'
+LABELS = {
+    'cardboard': 'cardboard',
+    'glass': 'glass',
+    'keyboard': 'e-waste',
+    'laptop': 'e-waste',
+    'metal': 'metal',
+    'mobile': 'e-waste',
+    'monitor': 'e-waste',
+    'mouse': 'e-waste',
+    'paper': 'paper',
+    'plastic': 'plastic',
+    'trash': 'trash',
+}
